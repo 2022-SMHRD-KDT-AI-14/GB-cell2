1e503ab364b9b33a05153ec1e51eb18477b63f0e
smhrd <smhrd@220.80.88.114>
2022-07-26 19:40:42 +0900

    박성결

---
+++ GB_Project/src/main/java/com/smhrd/model/ShareDAO.java
@@ -0,0 +1,29 @@
+package com.smhrd.model;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.database.SqlSessionManager;
+
+public class ShareDAO {
+
+	SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+	SqlSession sqlSession = sqlSessionFactory.openSession();
+	
+	public List<Share> selectAllMyList(String loginMember){
+		List<Share> list =null;
+		try {
+			//모든정보를 가져오려고하므로 인자필요없음.
+			list=sqlSession.selectList("com.smhrd.model.ShareDAO.selectAllMyList",loginMember); 
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+		return list;
+	}
+	
+	
+}
